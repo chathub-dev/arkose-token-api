646e5dac321ca713f5ac2571c1e2e0c3daa86509
wong2 <wonderfuly@gmail.com>
2023-06-28 12:36:56 +0800

    Add log & update path

--- main.go
+++ main.go
@@ -2,13 +2,16 @@ package main
 
 import (
     "github.com/gofiber/fiber/v2"
+    "github.com/gofiber/fiber/v2/middleware/logger"
     "github.com/flyingpot/funcaptcha"
 )
 
 func main() {
     app := fiber.New()
 
-    app.Get("/", func(c *fiber.Ctx) error {
+    app.Use(logger.New())
+
+    app.Get("/token", func(c *fiber.Ctx) error {
         token, err := funcaptcha.GetOpenAIToken()
         if err != nil {
             return c.JSON(fiber.Map{ "error": err.Error(), })
